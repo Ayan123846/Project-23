f05417de4893e80214624f188ad7494b49aecea8
Ayan123846 <66216467+Ayan123846@users.noreply.github.com>
2020-07-01 08:56:05 +0300

    Add files via upload

---
+++ Project 23/SupplyMission-master/sketch.js
@@ -0,0 +1,83 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var boxside1,boxside2,boxside3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload(){
+
+	helicopterIMG=loadImage("helicopter.png");
+	packageIMG=loadImage("package.png");
+}
+
+function setup() {
+	createCanvas(800,800);
+	rectMode(CENTER);
+	
+   	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+    groundSprite = createSprite(width/2,686, width,10);
+	groundSprite.shapeColor = color(255)
+
+    engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {
+		restitution:0.6, 
+		friction:0,
+		isStatic:true});
+
+		World.add(world, packageBody);
+
+		boxside1 = new Box(400,670,200,20);
+		boxside2 = new Box(310,630,20,100);
+		boxside3 = new Box(490,630,20,100);
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2,650, width, 10 , {
+	isStatic:true} );
+	
+	World.add(world,ground);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+
+  background(0);
+
+  textSize(25);
+  fill("white");
+  textFont("Times New Roman");
+  text("Press Down Arrow Key To Drop The Pacage",160,100);
+
+  packageSprite.x = packageBody.position.x;
+  packageSprite.y = packageBody.position.y; 
+
+  boxside1.display();
+  boxside2.display();
+  boxside3.display();
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
+
+
+
